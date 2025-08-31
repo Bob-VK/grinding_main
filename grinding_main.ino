8407ec94a7aeede3e34009a048aff7a5d57e792f
Bob-VK <vk-all@mail.ru>
2025-09-01 00:21:22 +0500

    не раб вариант. Послео тключения таймаута команды readString() режет

--- grinding_main.ino
+++ grinding_main.ino
@@ -4,13 +4,14 @@
 void setup() {
 
  Serial.begin(9600);
+ Serial.setTimeout(0);
  Serial.println(FPSTR(DebugON));
-   /*/ Настраиваем 8-й пин как выход Serial.begin TODO*/
+   /*/ Настраиваем 8-й пин как выход */
   pinMode(pin8, OUTPUT);
   pinMode(pin9, OUTPUT);
   /*  Настраиваем пин светодиода как выход */
   pinMode(ledPin, OUTPUT);
-
+  pinMode(pin10,  INPUT);
 }
 
 void loop() {
@@ -18,83 +19,95 @@ void loop() {
 {
   lenStrRead = Serial.available();
   //ждем когда буфер стабилизируется
-  if (( lenStrRead> 0)&&(lenStrRead == OldlenStrRead)){
+  if (( lenStrRead> )&&(lenStrRead == OldlenStrRead)){
 
+    if(CountStableRead<CountStableReadOk){
+      CountStableRead++;
+      //Serial.println(CountStableRead);
+    }else{
+        CountStableRead = 0;
         CmdStr = Serial.readString();
-        int PosSeparate  = CmdStr.indexOf(SEMICOLON);
-       if (PosSeparate != -1){
-          CmdParam = CmdStr.substring(PosSeparate+1,CmdStr.length()).toInt();
-          CmdStr=CmdStr.substring(0,PosSeparate);
-        } else{
-          CmdParam =0;
-          int LastChar =CmdStr.length() - 1;
-          if (CmdStr.charAt(LastChar)==10){//очистка на всякий сл
-              CmdStr=CmdStr.substring(0,LastChar);
-            }
-          }//if (PosSeparate != -1)
-       OldlenStrRead = 0;
-        CmdResult =  "Not Command";
+        if (CmdStr.equals(HBT)){
+            Serial.write('~');
+        }else{
+          
         
-       if  (CmdStr.equals(cmdDnOn)){ 
-          CmdResult =  CmdResultOK;
-          // Устанавливаем высокий уровень на 8-й пин
-          digitalWrite(pin8, HIGH);
-        
-          // Включаем встроенный светодиод
-          digitalWrite(ledPin, HIGH);          
-       }//if  (CmdStr.equals(cmdDnOn))
-       
-       else if  (CmdStr.equals(cmdDnOff)){ 
-          CmdResult =  CmdResultOK;
-          // Устанавливаем низкий уровень на 8-й пин
-          digitalWrite(pin8, LOW);
-        
-          // Выключаем встроенный светодиод
-          digitalWrite(ledPin, LOW);
-       }//if (CmdStr.equals(cmdDnOff)) 
-       else if  (CmdStr.equals(cmdUpOn)){ 
-          CmdResult =  CmdResultOK;
-          // Устанавливаем высокий уровень на 8-й пин
-          digitalWrite(pin9, HIGH);
-        
-          // Включаем встроенный светодиод
-          digitalWrite(ledPin, HIGH);          
-       }//if  (CmdStr.equals(cmdUpOn))
-       
-       else if  (CmdStr.equals(cmdUpOff)){ 
-          CmdResult =  CmdResultOK;
-          // Устанавливаем низкий уровень на 8-й пин
-          digitalWrite(pin9, LOW);
-        
-          // Выключаем встроенный светодиод
-          digitalWrite(ledPin, LOW);
-       }//if (CmdStr.equals(cmdUpOff))  
-      else  if  (CmdStr.equals(SetALLOWANCE)){ 
-          ALLOWANCE = CmdParam;
-          CmdResult =  CmdResultOK;
+          int PosSeparate  = CmdStr.indexOf(SEMICOLON);
+          if (PosSeparate != -1){
+              CmdParam = CmdStr.substring(PosSeparate+1,CmdStr.length()).toInt();
+              CmdStr=CmdStr.substring(0,PosSeparate);
+            } else{
+              CmdParam =0;
+              int LastChar =CmdStr.length() - 1;
+              if (CmdStr.charAt(LastChar)==10){//очистка на всякий сл
+                  CmdStr=CmdStr.substring(0,LastChar);
+                }
+              }//if (PosSeparate != -1)
+          OldlenStrRead = 0;
+            CmdResult =  "Not Command";
+            
+          if  (CmdStr.equals(cmdDnOn)){ 
+              CmdResult =  CmdResultOK;
+              // Устанавливаем высокий уровень на 8-й пин
+              digitalWrite(pin8, HIGH);
+            
+              // Включаем встроенный светодиод
+              digitalWrite(ledPin, HIGH);          
+          }//if  (CmdStr.equals(cmdDnOn))
+          
+          else if  (CmdStr.equals(cmdDnOff)){ 
+              CmdResult =  CmdResultOK;
+              // Устанавливаем низкий уровень на 8-й пин
+              digitalWrite(pin8, LOW);
+            
+              // Выключаем встроенный светодиод
+              digitalWrite(ledPin, LOW);
+          }//if (CmdStr.equals(cmdDnOff)) 
+          else if  (CmdStr.equals(cmdUpOn)){ 
+              CmdResult =  CmdResultOK;
+              // Устанавливаем высокий уровень на 8-й пин
+              digitalWrite(pin9, HIGH);
+            
+              // Включаем встроенный светодиод
+              digitalWrite(ledPin, HIGH);          
+          }//if  (CmdStr.equals(cmdUpOn))
+          
+          else if  (CmdStr.equals(cmdUpOff)){ 
+              CmdResult =  CmdResultOK;
+              // Устанавливаем низкий уровень на 8-й пин
+              digitalWrite(pin9, LOW);
+            
+              // Выключаем встроенный светодиод
+              digitalWrite(ledPin, LOW);
+          }//if (CmdStr.equals(cmdUpOff))  
+          else  if  (CmdStr.equals(SetALLOWANCE)){ 
+              ALLOWANCE = CmdParam;
+              CmdResult =  CmdResultOK;
 
-       }//if (CmdStr.equals(SetALLOWANCE)) 
-       else  if  (CmdStr.equals(SetPause)){
-          PauseMove = CmdParam;
-          CmdResult = CmdResultOK;
-       }// else  if  (CmdStr.equals(SetPause)) 
-       else  if  (CmdStr.equals(cmdStart)){
-          Start = true;
-          CurCountStep = ALLOWANCE;
-          CmdResult = CmdResultOK;
-       }//else  if  (CmdStr.equals(Start))
-                 
-          Serial.print(CmdStr);
-          Serial.print(":");
-          Serial.print(CmdParam,DEC);  
-          Serial.println(CmdResult);
-
-        }//if ( lenStrRead> 0)&&(lenStrRead == OldlenStrRead)
-    OldlenStrRead = lenStrRead;
-  
+          }//if (CmdStr.equals(SetALLOWANCE)) 
+          else  if  (CmdStr.equals(SetPause)){
+              PauseMove = CmdParam;
+              CmdResult = CmdResultOK;
+          }// else  if  (CmdStr.equals(SetPause)) 
+          else  if  (CmdStr.equals(cmdStart)){
+              Start = true;
+              CurCountStep = ALLOWANCE;
+              CmdResult = CmdResultOK;
+          }//else  if  (CmdStr.equals(Start))
+                    
+              Serial.print(CmdStr);
+              Serial.print(":");
+              Serial.print(CmdParam,DEC);  
+              Serial.println(CmdResult);
+        }//if (CmdStr.equals(HBT)) else
+            }//if ( lenStrRead> 0)&&(lenStrRead == OldlenStrRead)
+        
+      
+}
+OldlenStrRead = lenStrRead;
 }// end ---прием команд  -----  
-  endTime = micros(); // Сохраняем время окончания цикла
-  if ((loopDuration = endTime - startTime)>PauseMove)
+  CurTime = micros(); // Сохраняем время окончания цикла
+  if ((DurationMove = CurTime - EarlyTimeMove)>PauseMove)
   { 
     if (Start){
       if (CurCountStep>0) {
@@ -124,13 +137,16 @@ void loop() {
     {// печать продолжительности цикла
     // Выводим результат в последовательный монитор
     Serial.print("CyD: ");
-    Serial.print(loopDuration);
+    Serial.print(DurationMove);
     Serial.print(" -- ");
     Serial.print(PauseMove);   
     Serial.println(" ms");
-    startTime =  endTime;
+    EarlyTimeMove =  CurTime;
     }
-  }//if ((loopDuration = endTime - startTime)>PauseMove)
+  }//if ((DurationMove = CurTime - EarlyTimeMove)>PauseMove)
 
-  
+  if ((DurationdigitalRead = CurTime - EarlyTimeRead)>PausedigitalRead){
+      EarlyTimeRead =  CurTime;
+
+  }
 }
--- my_var.h
+++ my_var.h
@@ -1,15 +1,17 @@
 #ifndef my_var
 #define my_var
 unsigned long DnOnTime;
-unsigned long endTime, startTime;
-
-unsigned long loopDuration, PauseMove = 500000;
-
+unsigned long EarlyTimeRead,EarlyTimeMove, CurTime;
+unsigned int  CountStableRead = 0;
+#define       CountStableReadOk 20
+unsigned long DurationMove, PauseMove               = 500000;
+unsigned long DurationdigitalRead, PausedigitalRead = 500100;
 #define FPSTR(pstr) (const __FlashStringHelper *)(pstr)
 
 // Устанавливаем номера пинов
 const int pin8 = 8;
 const int pin9 = 9;
+const int pin10 = 10;
 const int ledPin = 13; // Встроенный светодиод на плате
 
 #define SEMICOLON ':'
@@ -20,6 +22,9 @@ const int ledPin = 13; // Встроенный светодиод на плат�
 #define SetALLOWANCE "allowance"
 #define SetPause "SetPause"
 #define cmdStart "Start"
+// #define HBT "HBT"
+#define HBT "~"
+
 
 #define CmdResultOK " OK"
 
